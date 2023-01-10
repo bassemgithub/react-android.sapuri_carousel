d6ca5b75c860a97885b37299c8b315cfc96f76fc
bassem <khilbassem@gmail.com>
2023-01-10 10:31:06 +0100

    clean code

--- src/contexts/ImagesContext.js
+++ src/contexts/ImagesContext.js
@@ -8,20 +8,6 @@ import img6 from '../images/6.jpg'
 import img7 from '../images/7.jpg'
 
 export const ImageContext = createContext();
-{/*
-class ImageContextProvider extends Component {
-  state = {
-    images :{img1, img2, img3, img5, img6, img7}
-  }
-  render() { 
-    return (
-      <ImageContext.Provider value={{...this.state}}>
-        {this.props.children}
-      </ImageContext.Provider>
-    );
-  }
-}
- */}
 
 const ImageContextProvider = (props) => {
   const [images, setImages] = useState([
